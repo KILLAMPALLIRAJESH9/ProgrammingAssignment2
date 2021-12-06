4b81af70b38d4171f234fa7cbd6b1fbb551e02bf
KILLAMPALLIRAJESH9 <95605841+KILLAMPALLIRAJESH9@users.noreply.github.com>
2021-12-06 12:44:40 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,15 +3,15 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(i = matrix()) {
-        inv <- NULL
+makeCacheMatrix <- function(i = matrix()) {                     #supplying matrix to i
+        inv <- NULL                                             
         set <- function(j) {
-                i <<- j
+                i <<- j                       # setting the value of the matrix
                 inv <<- NULL
   }
-  get <- function() i
-  setinverse <- function(inverse) inv <<- inverse
-  getinverse <- function() inv
+  get <- function() i                        # get the velue of matrix i
+  setinverse <- function(inverse) inv <<- inverse       # setting the value of matrix i and make it inverse
+  getinverse <- function() inv                          # get the inverse matrix
   list(set = set, get = get,
                 setinverse = setinverse,
                 getinverse = getinverse)
@@ -21,12 +21,12 @@ makeCacheMatrix <- function(i = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(i, ...) {
-  inv <- i$getinverse()
-  if(!is.null(inv)) {
-    message("getting cached data")
+  inv <- i$getinverse()                     
+  if(!is.null(inv)) {                           # checking the inversion is made 
+    message("getting cached data")              
     return(inv)
   }
-  matrix_to_invert <- i$get()
+  matrix_to_invert <- i$get()                  
   inv <- solve(matrix_to_invert, ...)
   i$setinverse(inv)
   inv      ## Return a matrix that is the inverse of 'i'
